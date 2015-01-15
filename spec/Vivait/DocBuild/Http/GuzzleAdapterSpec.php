19374fc79a8fd7c97dc70c1af88de892614c461f
Robin Cawser <robin@vivait.co.uk>
2015-01-15 09:30:34 +0000

    Add more specs for get/post for guzzle adapter

--- spec/Vivait/DocBuild/Http/GuzzleAdapterSpec.php
+++ spec/Vivait/DocBuild/Http/GuzzleAdapterSpec.php
@@ -5,6 +5,7 @@ namespace spec\Vivait\DocBuild\Http;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Message\Request;
 use GuzzleHttp\Message\Response;
+use GuzzleHttp\Message\ResponseInterface;
 use GuzzleHttp\Stream\Stream;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -114,18 +115,78 @@ class GuzzleAdapterSpec extends ObjectBehavior
         $response = new Response(200);
         $response->setBody(Stream::factory(""));
 
-        $client->get($url, [
-            'exceptions' => false,
-            'query' => [],
-            'headers' => ['Auth' => 'myapikey'],
-        ])->willReturn($response);
+        $client->get(
+            $url,
+            [
+                'exceptions' => false,
+                'query' => [],
+                'headers' => ['Auth' => 'myapikey'],
+            ]
+        )->shouldBeCalled()->willReturn($response);
 
         $this->get('documents', [], ['Auth' => 'myapikey']);
     }
 
-    function it_can_perform_a_post_request()
+    function it_can_perform_a_get_request_with_query(ClientInterface $client)
     {
+        $url = 'http://doc.build/api/documents';
+        $response = new Response(200);
+        $response->setBody(Stream::factory(""));
 
+        $client->get(
+            $url,
+            [
+                'exceptions' => false,
+                'query' => ['id' => '1234'],
+                'headers' => ['Auth' => 'myapikey'],
+            ]
+        )->shouldBeCalled()->willReturn($response);
+
+        $this->get('documents', ['id' => '1234'], ['Auth' => 'myapikey']);
+    }
+
+    function it_can_perform_a_post_request(ClientInterface $client)
+    {
+        $url = 'http://doc.build/api/documents';
+        $response = new Response(200);
+        $response->setBody(
+            Stream::factory(
+                '{"status":0, "id":"a1ec0371-966d-11e4-baee-08002730eb8a", "name":"Test Document 1", "extension":"docx" }'
+            )
+        );
+
+        $client->post(
+            $url,
+            [
+                'exceptions' => false,
+                'body' => ['document[name]' => 'Test File 1', 'document[extension]' => 'docx'],
+                'headers' => ['Auth' => 'myapikey'],
+            ]
+        )->shouldBeCalled()->willReturn($response);
+
+        $this->post('documents', ['document[name]' => 'Test File 1', 'document[extension]' => 'docx'], ['Auth' => 'myapikey']);
+    }
+
+    function it_can_perform_a_post_request_with_parameters(ClientInterface $client)
+    {
+        $url = 'http://doc.build/api/documents';
+        $response = new Response(200);
+        $response->setBody(Stream::factory(""));
+
+        $client->post(
+            $url,
+            [
+                'exceptions' => false,
+                'body' => ['document[name]' => 'Test File 1', 'document[extension]' => 'docx'],
+                'headers' => ['Auth' => 'myapikey'],
+            ]
+        )->shouldBeCalled()->willReturn($response);
+
+        $this->post(
+            'documents',
+            ['document[name]' => 'Test File 1', 'document[extension]' => 'docx'],
+            ['Auth' => 'myapikey']
+        );
     }
 
     function it_does_not_throw_exceptions_for_http_errors()
